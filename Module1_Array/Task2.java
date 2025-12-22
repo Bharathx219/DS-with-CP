b940d7344440ecff249ee3056ae422c44855efea
Bharath <bharathx219@gmail.com>
2025-12-22 10:44:03 +0530

    your message

---
+++ Module1_Array/Task2.java
@@ -0,0 +1,24 @@
+class Main {
+
+    String name;
+    int age;
+
+    void getDetails() {
+        System.out.println(name);
+        System.out.println(age);
+    }
+
+    public static void main(String[] args) {
+
+        Main a = new Main();
+        a.name = "Bharath";
+        a.age = 20;
+
+        // first time
+        System.out.println(a.name);
+        System.out.println(a.age);
+
+        // second time
+        a.getDetails();
+    }
+}
